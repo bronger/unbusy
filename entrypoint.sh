f4cd26567051add989fdcc95a162498b24f311dc
Torsten Bronger <bronger@physik.rwth-aachen.de>
2021-12-08 10:57:34 +0100

    Improve check for invalid value

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+printf %s "$START_JITTER_SEC" | grep -q "^[0-9]\+$" || exit 13
+
 waiting_sec=10
 cycles_min=`expr "\$TIMEOUT_SEC_MIN" / $waiting_sec` || exit 2
 cycles_max=`expr "\$TIMEOUT_SEC_MAX" / $waiting_sec` || exit 3
@@ -12,6 +14,5 @@ do
     cycles_left=`expr $cycles_left - 1` || exit 8
 done
 
-start_jitter_sec=`expr "\$START_JITTER_SEC" / 1` || exit 9
 jitter_sec=`awk "BEGIN{srand(); print int(rand()*($start_jitter_sec+1))}"` || exit 10
 sleep $jitter_sec || exit 11
