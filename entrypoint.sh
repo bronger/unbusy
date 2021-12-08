79df69c7f069475635849bcb75eabf218360b58c
Torsten Bronger <bronger@physik.rwth-aachen.de>
2021-12-08 10:41:43 +0100

    Simplify code

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,7 @@ cycles_min=`expr "\$TIMEOUT_SEC_MIN" / $waiting_sec` || exit 2
 cycles_max=`expr "\$TIMEOUT_SEC_MAX" / $waiting_sec` || exit 3
 cycles_left=`awk -v min=$cycles_min -v max=$cycles_max 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'` || exit 4
 while
-    load=`cat /proc/loadavg` || exit 5
-    too_big=`echo $load | awk "{ print (\\$1 > $LOAD_MAX) ; }"` || exit 6
+    too_big=`awk "{print (\\$1 > $LOAD_MAX)}" /proc/loadavg` || exit 6
     [ $cycles_left != 0 -a $too_big = 1 ]
 do
     sleep 10 || exit 7
